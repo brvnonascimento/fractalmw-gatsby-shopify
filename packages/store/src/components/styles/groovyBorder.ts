c7c2ef64d1b4738ee9111ae282221cdb18b4d1c0
brvno <bhenrique.contact@gmail.com>
2020-10-13 13:57:50 -0300

    refactor: modularize groove border props

---
+++ packages/store/src/components/styles/groovyBorder.ts
@@ -0,0 +1,8 @@
+export const groovyBorder = {
+  borderRadius: '10px',
+  border: '1px',
+  borderStyle: 'groove',
+  borderWidth: '2px',
+  borderColor: 'rgba(0, 0, 0, 0.5)',
+  boxShadow: '6px 4px 0px rgba(240, 52, 52, 0.4)'
+}
