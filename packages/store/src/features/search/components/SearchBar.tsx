f31b6ebc46905eb7bffe25480e6fbec2a67f7fba
brvno <bhenrique.contact@gmail.com>
2020-10-21 23:28:58 -0300

    feat: add Search feature

---
+++ packages/store/src/features/search/components/SearchBar.tsx
@@ -0,0 +1,108 @@
+import React, { ReactEventHandler, useEffect, useState } from 'react'
+import {
+  Box,
+  BoxProps,
+  Divider,
+  Flex,
+  Icon,
+  Image,
+  Input,
+  InputLeftElement,
+  Text
+} from '@chakra-ui/core'
+import { groovyBorder } from '../../../components/styles/groovyBorder'
+import { Link } from 'gatsby'
+
+export interface SearchResult {
+  title: string
+  image: {
+    alt: string
+    src: string
+    fallbackSrc: string
+  }
+  handle: string
+}
+
+interface SearchBarProps extends BoxProps {
+  searchResults: SearchResult[]
+  handleSearch: (search: string) => void
+}
+
+export const SearchBar = ({
+  searchResults = [],
+  handleSearch,
+  ...props
+}: SearchBarProps) => {
+  const [search, setSearch] = useState('')
+
+  const isSearching = search.trim().length !== 0
+
+  useEffect(() => {
+    if (isSearching) {
+      handleSearch(search)
+    }
+  }, [search])
+
+  return (
+    <Box
+      as="section"
+      role="search"
+      borderWidth={0}
+      color={'grey'}
+      position={'relative'}
+      display={'flex'}
+      alignItems={'center'}
+      {...props}
+    >
+      <InputLeftElement
+        top={'9px'}
+        alignSelf={'center'}
+        left={'18px'}
+        children={<Icon name="search" color="gray.300" />}
+      />
+      <Input
+        borderRadius={0}
+        paddingLeft={'40px'}
+        background="#FFFF"
+        type="search"
+        placeholder="Pesquisar"
+        onChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+          setSearch(target.value)
+        }
+        {...groovyBorder}
+      />
+      {isSearching && (
+        <Flex
+          width={'100%'}
+          height={'auto'}
+          flexDirection={'column'}
+          position={'absolute'}
+          zIndex={4}
+          background={'white'}
+          top={'50px'}
+          {...groovyBorder}
+        >
+          {searchResults.map(
+            ({ title, image: { alt, src, fallbackSrc }, handle }) => (
+              <Link to={`/produto/${handle}`}>
+                <Flex>
+                  <Image
+                    width={'100px'}
+                    alt={alt}
+                    src={src}
+                    fallbackSrc={fallbackSrc}
+                    mx="10px"
+                  />
+                  <Text fontSize={'bold'} color="black" fontSize={'md'}>
+                    {title}
+                  </Text>
+                  <Divider />
+                </Flex>
+              </Link>
+            )
+          )}
+        </Flex>
+      )}
+    </Box>
+  )
+}
---
+++ packages/store/src/features/search/hooks/useSearch.ts
@@ -0,0 +1,82 @@
+import { gql, useLazyQuery } from '@apollo/client'
+import { useMemo } from 'react'
+import { SearchResult } from '../components/SearchBar'
+
+const SEARCH_SHIRT_QUERY = gql`
+  query SearchShirtQuery($first: Int!, $query: String) {
+    products(first: $first, query: $query) {
+      edges {
+        node {
+          title
+          handle
+          images(first: 1) {
+            edges {
+              node {
+                src: transformedSrc(
+                  maxHeight: 200
+                  maxWidth: 200
+                  preferredContentType: WEBP
+                )
+                fallbackSrc: transformedSrc(
+                  maxHeight: 200
+                  maxWidth: 200
+                  preferredContentType: JPG
+                )
+                altText
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+type UseSearch = [
+  (first: number, query: string) => void,
+  { searchResults: SearchResult[]; error: any; loading: boolean }
+]
+
+export const useSearch = (): UseSearch => {
+  const [searchShirt, { data, error, loading }] = useLazyQuery(
+    SEARCH_SHIRT_QUERY
+  )
+
+  const handleSearch = (first: number, query: string) => {
+    searchShirt({
+      variables: {
+        first,
+        query
+      }
+    })
+  }
+
+  console.log(data)
+
+  const searchResults: SearchResult[] = useMemo(
+    () =>
+      data
+        ? data.products.edges.map(
+            ({ node: { title, handle, images } }: any) => {
+              const { src, fallbackSrc, altText } = images.edges[0].node
+              console.log(images.edges[0].node)
+              return {
+                title,
+                handle,
+                image: { src, fallbackSrc, altText }
+              }
+            }
+          )
+        : [],
+    [data]
+  )
+
+  return [
+    handleSearch,
+    {
+      searchResults,
+      error,
+      loading
+    }
+  ]
+}
